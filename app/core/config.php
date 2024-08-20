b5f4864d9a0d904ffbebfc79afeda9045a0585c6
D.N.Kodithuwakku <140165465+DilshaniNK@users.noreply.github.com>
2024-08-20 10:07:22 +0530

    define a constan in config file

--- app/core/config.php
+++ app/core/config.php
@@ -1,5 +1,17 @@
 <?php
+
+if($_SERVER['SERVER_NAME'] == 'localhost')
+{
+    
     define('ROOT','http://localhost/mvc/public');//define the constant to identify actual path of images or something
+}else
+
+{
+    
     define('ROOT', 'https://www.yourwebsite.com');//for if it is we log throught the online website
+
+}
+    
+   
     
 
